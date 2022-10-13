5eaf23b23b0f1012b56aa4a2cf939811af4e8e9d
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-10-12 21:11:19 -0400

    Updated files Index, Intern, Engineer, Employee, Manager

---
+++ index.js
@@ -0,0 +1,20 @@
+const Inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+
+class Inquirer {
+
+}
+
+class Employee {
+    
+}
+
+class Intern {
+    
+}
+
+class Manager {
+    
+}
