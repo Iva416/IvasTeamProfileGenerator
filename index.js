6b24eda3a1240a0303871ed9058417921ecad6b6
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-10-14 21:21:13 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
-const Inquirer = require("inquirer");
+const fs = require('fs')
+const inquirer = require('inquirer');
+
+
+const GenerateHtml = require("./src/generateHTML")
+
 const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 
-const GenerateHtml(){
+const GenerateHtml();
+
+let content = ""
+
+function employeePrompt = () =>
+
+//What is the Employees Name?
+//What is the Employees Id?
+//fswrite file to create team
+//ask if you want to add manager intern or engineer
+//ask if the team is done
+//process info to template 
 
 
-class Inquirer {
+function engineerPrompt = () =>
 
-}
+inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: "What is the Employee NameEngineer?",
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: "What is the Employee id?",
+                name: 'id',
+            },        
+            {
+                type: 'input',
+                message: "What is the the Employee's email?",
+                name: 'email',
+            },       
+            {
+                type: 'input',
+                message: "What is your github Username?",
+                name: 'githubUsername',
+            },
+        ])
 
-class Employee {
-    
-}
+    .then((data) => {
+        
+ })
 
-class Intern {
-    
-}
-
-class Manager {
-    
-}
-
-//get name get id get email
---
+++ tests/Employee.test.js
@@ -0,0 +1,36 @@
+tesconst Employee = require ("../lib/Employee")
+const name = "Iva"
+const id = 101
+const email = "iva123@gmail.com"
+const obj = new Employee(name, id, email)
+
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should have Employee Name", () => {
+    
+     expect(obj).toBeInstanceOf(Employee);
+     expect(obj.name).toBe(name);
+     expect(obj.id).toBe(id);
+     expect(obj.email).toBe(email)
+        })
+
+     describe("getName", () => {
+        it("should return Employee", () => {
+            expect(obj.getName()).toBe(obj.name);
+        })
+        })
+
+    describe("getId", () => {
+        it("should return Employee Id", () => {
+            expect(obj.getId()).toBe(obj.id);    
+        })
+        })
+    describe("getEmail", () => {
+        it("should return Employee Email", () => {
+            expect(obj.getEmail()).toBe(obj.email);
+        })
+        })
+
+     });
+    })
